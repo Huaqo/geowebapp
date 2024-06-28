79bfac77ceea05faee7e9e87c29a1d9d14688687
huaqo <mail@huaqo.com>
2024-06-28 12:05:36 +0200

    lol

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import os
 from decimal import Decimal
 
 ## QUERIES
-
 DATASET = """
 SELECT {attributes}
 FROM Hochschulen h
@@ -25,7 +24,6 @@ WHERE {search_attr} ILIKE :search
 ORDER BY {sort_by} {order}
 LIMIT {limit};
 """
-
 DATASET_GROUPED = """
 SELECT {group_by}, COUNT(*) AS Anzahl
 FROM Hochschulen
@@ -34,26 +32,19 @@ ORDER BY Anzahl DESC;
 """
 
 ## INIT
-
-
 db = SQLAlchemy()
-
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://huaqo:0000@localhost:5432/geowebapp')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 def create_app():
     template_dir = os.path.abspath('.')
     app = Flask(__name__,template_folder=template_dir)
     app.config.from_object(Config)
     db.init_app(app)
     return app
-
 app = create_app()
 
-
 ## DATA QUERY 
-
 class Dataset_Parameters:
     def __init__(self, order='ASC', limit='10', sort_by='gjahr', attributes=['name_h'], search='', search_attr='name_h', group_by='typ'):
         if attributes is None:
@@ -89,7 +80,6 @@ class Dataset_Parameters:
         if 'lon' not in parameters.attributes:
             parameters.attributes.append('lon')
         return parameters
-
 def get_data(query_template, parameters):
     params_dict = {k: v for k, v in parameters.__dict__.items()}
     for key, value in params_dict.items():
@@ -101,7 +91,6 @@ def get_data(query_template, parameters):
     return result.fetchall()
 
 ## TABLES
-
 def create_table(data, parameters):
     lat_index = parameters.attributes.index('lat') if 'lat' in parameters.attributes else -1
     lon_index = parameters.attributes.index('lon') if 'lon' in parameters.attributes else -1
@@ -114,14 +103,12 @@ def create_table(data, parameters):
     filtered_attributes = [attr for attr in parameters.attributes if attr not in ['lat', 'lon']]
     table = tabulate(filtered_data, headers=filtered_attributes, tablefmt='html')
     return table
-
 def create_grouped_table(data, parameters):
     headers = [parameters.group_by, 'Anzahl']
     table = tabulate(data, headers=headers, tablefmt='html')
     return table
 
 ## MAP
-    
 def generate_map(data, column_names):
     map_center = [51.1657, 10.4515]
     map = folium.Map(location=map_center, tiles="cartodb positron", zoom_start=6)
@@ -151,10 +138,7 @@ def generate_map(data, column_names):
     map_html = map._repr_html_()
     return map_html
 
-
-
 ## VIEWS
-
 @app.route('/', methods=['GET'])
 def home():
     dataset_parameters = Dataset_Parameters.get_parameters(request)
@@ -165,6 +149,7 @@ def home():
     map = generate_map(dataset, dataset_parameters.attributes)
     return render_template('index.html', table=table, table_grouped=table_grouped, parameters=dataset_parameters, map=map)
 
+## RUN
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
