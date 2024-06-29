d682899f21e33be2db1e4fa1acd104f9db624ad6
huaqo <mail@huaqo.com>
2024-06-29 11:59:38 +0200

    apfel

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ import folium
 import os
 from decimal import Decimal
 
+## INIT
+db = SQLAlchemy()
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://huaqo:0000@localhost:5432/geowebapp')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+def create_app():
+    template_dir = os.path.abspath('.')
+    app = Flask(__name__,template_folder=template_dir)
+    app.config.from_object(Config)
+    db.init_app(app)
+    return app
+app = create_app()
+
 ## QUERIES
 DATASET = """
 SELECT {attributes}
@@ -31,18 +44,76 @@ GROUP BY {group_by}
 ORDER BY Anzahl DESC;
 """
 
-## INIT
-db = SQLAlchemy()
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://huaqo:0000@localhost:5432/geowebapp')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-def create_app():
-    template_dir = os.path.abspath('.')
-    app = Flask(__name__,template_folder=template_dir)
-    app.config.from_object(Config)
-    db.init_app(app)
-    return app
-app = create_app()
+## PARAMETERS
+order_by_options = [
+    {'value': 'land', 'label': 'Bundesland'},
+    {'value': 'typ', 'label': 'Hochschultyp'},
+    {'value': 'traeger', 'label': 'Träger'},
+    {'value': 'precht', 'label': 'Promotionsrecht'},
+    {'value': 'hrecht', 'label': 'Habilitationsrecht'}
+]
+
+shortcut_options = [
+    {'url': '//127.0.0.1:5000/?search_attr=typ&search=&order=ASC&sort_by=gjahr&limit=10&attributes=&attributes=h.name_h&attributes=h.gjahr&attributes=h.ort&generate_map=true', 'label': 'Älteste Hochschulen in Deutschland'},
+    {'url': '//127.0.0.1:5000/?search_attr=typ&search=&order=DESC&sort_by=gjahr&limit=10&attributes=&attributes=h.name_h&attributes=h.gjahr&attributes=h.ort&generate_map=true', 'label': 'Jüngste Hochschulen in Deutschland'},
+    {'url': '//127.0.0.1:5000/?search_attr=typ&search=&order=DESC&sort_by=studenten&limit=10&attributes=&attributes=h.name_h&attributes=h.studenten&attributes=h.ort&generate_map=true', 'label': 'Hochschulen mit den meisten Studenten'},
+    {'url': '//127.0.0.1:5000/?search_attr=h.land&search=Berlin&order=DESC&sort_by=studenten&limit=10&attributes=&attributes=h.name_h&attributes=h.studenten&generate_map=true', 'label': 'Berliner Hochschulen mit den meisten Studenten'},
+    {'url': '//127.0.0.1:5000/?search_attr=typ&search=&order=ASC&sort_by=studenten&limit=10&attributes=&attributes=h.name_h&attributes=h.studenten&attributes=h.ort&generate_map=true', 'label': 'Hochschulen mit den wenigsten Studenten'},
+    {'url': '//127.0.0.1:5000/?search_attr=typ&search=&order=DESC&sort_by=b.total_population&limit=10&attributes=&attributes=h.name_h&attributes=h.ort&attributes=b.total_population&generate_map=true', 'label': 'Hochschulen mit den meisten Einwohnern'},
+    {'url': '//127.0.0.1:5000/?search_attr=typ&search=&order=ASC&sort_by=b.total_population&limit=10&attributes=&attributes=h.name_h&attributes=h.ort&attributes=b.total_population&generate_map=true', 'label': 'Hochschulen mit den wenigsten Einwohnern'},
+    {'url': '//127.0.0.1:5000/?search_attr=typ&search=&order=ASC&sort_by=m.mieten_ab_2019&limit=100&attributes=&attributes=h.name_h&attributes=h.ort&attributes=m.mieten_ab_2019&generate_map=true', 'label': 'Hochschulen mit den niedrigsten Mieten'},
+    {'url': '//127.0.0.1:5000/?search_attr=typ&search=&order=DESC&sort_by=m.mieten_ab_2019&limit=200&attributes=&attributes=h.name_h&attributes=h.ort&attributes=m.mieten_ab_2019&generate_map=true', 'label': 'Hochschulen mit den höchsten Mieten'}
+]
+
+search_options = [
+    {'value': 'h.name_h', 'label': 'Hochschulname'},
+    {'value': 'h.typ', 'label': 'Typ'},
+    {'value': 'h.traeger', 'label': 'Träger'},
+    {'value': 'h.land', 'label': 'Land'},
+    {'value': 'h.precht', 'label': 'Precht'},
+    {'value': 'h.hrecht', 'label': 'Hrecht'},
+    {'value': 'h.str', 'label': 'Straße'},
+    {'value': 'h.ort', 'label': 'Ort'},
+    {'value': 'h.web', 'label': 'Web'}
+]
+
+order_options = [
+    {'value': 'ASC', 'label': 'Aufsteigend'},
+    {'value': 'DESC', 'label': 'Absteigend'}
+]
+
+sort_by_options = [
+    {'value': 'name_h', 'label': 'Hochschulname'},
+    {'value': 'gjahr', 'label': 'Gründungsjahr'},
+    {'value': 'typ', 'label': 'Typ'},
+    {'value': 'traeger', 'label': 'Träger'},
+    {'value': 'land', 'label': 'Land'},
+    {'value': 'studenten', 'label': 'Studenten'},
+    {'value': 'precht', 'label': 'Precht'},
+    {'value': 'hrecht', 'label': 'Hrecht'},
+    {'value': 'plz', 'label': 'PLZ'},
+    {'value': 'str', 'label': 'Straße'},
+    {'value': 'ort', 'label': 'Ort'},
+    {'value': 'b.total_population', 'label': 'Einwohner'},
+    {'value': 'm.mieten_ab_2019', 'label': 'Mieten'}
+]
+
+checkbox_options = [
+    {'value': 'h.name_h', 'label': 'Hochschulname'},
+    {'value': 'h.gjahr', 'label': 'Gründungsjahr'},
+    {'value': 'h.typ', 'label': 'Typ'},
+    {'value': 'h.traeger', 'label': 'Träger'},
+    {'value': 'h.land', 'label': 'Land'},
+    {'value': 'h.studenten', 'label': 'Studenten'},
+    {'value': 'h.precht', 'label': 'Precht'},
+    {'value': 'h.hrecht', 'label': 'Hrecht'},
+    {'value': 'h.plz', 'label': 'PLZ'},
+    {'value': 'h.str', 'label': 'Straße'},
+    {'value': 'h.ort', 'label': 'Ort'},
+    {'value': 'h.web', 'label': 'Web'},
+    {'value': 'b.total_population', 'label': 'Einwohner'},
+    {'value': 'm.mieten_ab_2019', 'label': 'Mieten'}
+]
 
 ## DATA QUERY 
 class Dataset_Parameters:
@@ -147,7 +218,18 @@ def home():
     table = create_table(dataset, dataset_parameters)
     table_grouped = create_grouped_table(dataset_grouped, dataset_parameters)
     map = generate_map(dataset, dataset_parameters.attributes)
-    return render_template('index.html', table=table, table_grouped=table_grouped, parameters=dataset_parameters, map=map)
+    return render_template(
+        'index.html', 
+        table=table, 
+        table_grouped=table_grouped, 
+        parameters=dataset_parameters, 
+        map=map,
+        order_by_options=order_by_options,
+        search_options=search_options,
+        order_options=order_options,
+        sort_by_options=sort_by_options,
+        shortcut_options=shortcut_options,
+        checkbox_options=checkbox_options)
 
 ## RUN
 if __name__ == '__main__':
